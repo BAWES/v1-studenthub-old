54ecd8666245c06b83deab4976314c6590c1902a
Khalid Al-Mutawa <contact@bawes.net>
2015-04-06 10:06:23 +0300

    Updated with production db settings

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=studenthub',
+            'username' => 'studenthubuser',
+            'password' => 'jxwMFGBJb3LzrypS',
             'charset' => 'utf8',
         ],
         'mailer' => [
