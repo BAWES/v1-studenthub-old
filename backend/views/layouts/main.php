38f69fe8311032d7bca10c7cc7791282178a7f6b
Khalid Al-Mutawa <contact@bawes.net>
2015-04-07 11:50:30 +0300

    Navigation adjustment for backend

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -32,18 +32,22 @@ AppAsset::register($this);
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
             ]);
-            $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-            ];
+            $menuItems = [];
+            
             if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
             } else {
-                $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->admin_name . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
+                $menuItems = 
+                [
+                    ['label' => 'Home', 'url' => ['/site/index']],
+                    [
+                        'label' => 'Logout (' . Yii::$app->user->identity->admin_name . ')',
+                        'url' => ['/site/logout'],
+                        'linkOptions' => ['data-method' => 'post']
+                    ]
                 ];
             }
+            
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => $menuItems,
