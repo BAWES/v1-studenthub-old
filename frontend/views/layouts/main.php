dbf69117ab882e9160b8d2ce18bcb03c6b8183d7
Khalid Al-Mutawa <contact@bawes.net>
2015-04-08 15:04:21 +0300

    Frontend preparations

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -66,7 +66,7 @@ AppAsset::register($this);
     <footer class="footer">
         <div class="container">
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right">Built Awesome</p>
         </div>
     </footer>
 
