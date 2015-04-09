b0a1ba4f0d89a1e405bd31e2010e3a735a2c7c9b
Khalid Al-Mutawa <contact@bawes.net>
2015-04-09 11:01:21 +0300

    Additional layout adjustments

--- frontend/views/site/test.php
+++ frontend/views/site/test.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+//Yii::$app->session->setFlash('error', 'There was an error sending email.');
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
