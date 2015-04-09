2b1e5afb9dae81a2a7fc438d522b90506f6682d8
BAWES <BAWES@192.168.1.107>
2015-04-09 19:25:04 +0300

    Removed most webarch template files

--- frontend/assets/TemplateAsset.php
+++ frontend/assets/TemplateAsset.php
@@ -16,39 +16,13 @@ class TemplateAsset extends AssetBundle
     
     //CSS will be added before closing </head> tag
     public $css = [
-        //BEGIN CORE CSS FRAMEWORK
-        'plugins/boostrapv3/css/bootstrap.min.css',
-        'plugins/boostrapv3/css/bootstrap-theme.min.css',
-        'plugins/font-awesome/css/font-awesome.css',
-        'css/animate.min.css',
-        'plugins/jquery-scrollbar/jquery.scrollbar.css',
-        
-        //BEGIN CSS TEMPLATE
-        'css/style.css',
-        'css/responsive.css',
-        'css/custom-icon-set.css',
-        
-        //Pace css for loading bars
-        'plugins/pace/pace-theme-flash.css',
     ];
     
     
     //JS will be added before closing </body> tag
     public $js = [
-        //BEGIN CORE JS FRAMEWORK
-        'plugins/boostrapv3/js/bootstrap.min.js',
-        'plugins/breakpoints.js',
-        'plugins/jquery-unveil/jquery.unveil.min.js',
-        'plugins/jquery-scrollbar/jquery.scrollbar.min.js',
-        
-        //BEGIN CORE TEMPLATE JS
-        'js/core.js',
-        
-        //Pace.js for loading bars
-        'plugins/pace/pace.min.js',
-        //Animate numbers
-        'plugins/jquery-numberAnimate/jquery.animateNumbers.js',
     ];
+    
     public $depends = [
         'yii\web\YiiAsset',
     ];
