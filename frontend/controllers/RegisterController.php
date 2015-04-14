09891c95e0f968791dafbac9dba612623ff7f48e
Khalid Al-Mutawa <contact@bawes.net>
2015-04-14 09:27:59 +0300

    Controller behaviors

--- frontend/controllers/RegisterController.php
+++ frontend/controllers/RegisterController.php
@@ -2,8 +2,41 @@
 
 namespace frontend\controllers;
 
+use yii\filters\AccessControl;
+
 class RegisterController extends \yii\web\Controller
 {
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['?'], //only allow unauthenticated users to register
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+    
+    
     public function actionIndex()
     {
         return $this->render('index');
