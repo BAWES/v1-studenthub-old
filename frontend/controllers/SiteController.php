7b988350556677d104e6be3bbb31cbb26b7951e3
Khalid Al-Mutawa <contact@bawes.net>
2015-04-28 08:25:40 +0300

    commented test action

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -90,8 +90,11 @@ class SiteController extends Controller
     public function actionTest()
     {
         //Test activation email
+        /*
         $student = \frontend\models\Student::findOne(13);
         $student->sendVerificationEmail();
+         * 
+         */
     }
 
     public function actionLogout()
