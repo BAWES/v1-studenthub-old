3d30ff562e848b975384c9e4af2b205fd8758681
Khalid Al-Mutawa <contact@bawes.net>
2015-04-28 09:27:47 +0300

    Updated site controllers

--- employer/controllers/SiteController.php
+++ employer/controllers/SiteController.php
@@ -20,13 +20,15 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
+                'only' => ['logout', 'register'],
                 'rules' => [
                     [
-                        'actions' => ['login', 'error'],
+                        'actions' => ['register'],
                         'allow' => true,
+                        'roles' => ['?'],
                     ],
                     [
-                        'actions' => ['logout', 'index'],
+                        'actions' => ['logout'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -57,6 +59,25 @@ class SiteController extends Controller
     {
         return $this->render('index');
     }
+    
+    
+    public function actionRegister()
+    {
+        $model = new \common\models\Employer();
+        
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }
+
+        return $this->render('register', [
+            'model' => $model,
+        ]);
+    }
+    
 
     public function actionLogin()
     {
@@ -79,5 +100,84 @@ class SiteController extends Controller
         Yii::$app->user->logout();
 
         return $this->goHome();
+    }
+    
+    public function actionRequestPasswordReset()
+    {
+        $model = new PasswordResetRequestForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->getSession()->setFlash('success', 'Check your email for further instructions.');
+
+                return $this->goHome();
+            } else {
+                Yii::$app->getSession()->setFlash('error', 'Sorry, we are unable to reset password for email provided.');
+            }
+        }
+
+        return $this->render('requestPasswordResetToken', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionResetPassword($token)
+    {
+        try {
+            $model = new ResetPasswordForm($token);
+        } catch (InvalidParamException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
+            Yii::$app->getSession()->setFlash('success', 'New password was saved.');
+
+            return $this->goHome();
+        }
+
+        return $this->render('resetPassword', [
+            'model' => $model,
+        ]);
+    }
+    
+    //set language to English
+    public function actionEnglish()
+    {
+        $cookies = Yii::$app->response->cookies;
+        $cookies->remove('language');
+        $cookies->add(new \yii\web\Cookie([
+            'name' => 'language',
+            'value' => 'en-US',
+        ]));
+        
+        //If User logged in, update his language preference
+        if (Yii::$app->user->id) {
+            $user = \common\models\Employer::findOne((int) Yii::$app->user->id);
+            $user->employer_language_pref = 'en-US';
+            $user->save();
+        }
+        
+        //return to previous page before language selection
+        return $this->redirect(Yii::$app->request->referrer);
+    }
+    
+    //set language to Arabic
+    public function actionArabic()
+    {
+        $cookies = Yii::$app->response->cookies;
+        $cookies->remove('language');
+        $cookies->add(new \yii\web\Cookie([
+            'name' => 'language',
+            'value' => 'ar-KW',
+        ]));
+        
+        //If User logged in, update his language preference
+        if (Yii::$app->user->id) {
+            $user = \common\models\Employer::findOne((int) Yii::$app->user->id);
+            $user->employer_language_pref = 'ar-KW';
+            $user->save();
+        }
+        
+        //return to previous page before language selection
+        return $this->redirect(Yii::$app->request->referrer);
     }
 }
--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -25,13 +25,8 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
+                'only' => ['logout'],
                 'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
                     [
                         'actions' => ['logout'],
                         'allow' => true,
