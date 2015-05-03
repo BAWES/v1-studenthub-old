baffeed87f5f5ee7bf1ef758fdd28eaa360d2820
Khalid Al-Mutawa <contact@bawes.net>
2015-05-03 11:35:59 +0300

    Translation adjustments

--- employer/views/site/login.php
+++ employer/views/site/login.php
@@ -9,23 +9,43 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = Yii::t("employer", 'Login');
 $this->params['breadcrumbs'][] = $this->title;
+
+$fieldTemplate = "{label}\n{beginWrapper}\n"
+                        . "<div class='inputer'>\n<div class='input-wrapper'>\n"
+                        . "{input}"
+                        . "</div>\n</div>\n{hint}\n{error}\n"
+                        . "{endWrapper}";
 ?>
 <div class="panel">
+    <div class="panel-heading">
+        <div class="panel-title">
+            <h4><?= Yii::t("employer", "Please fill out the following fields to login") ?></h4>
+        </div>
+    </div>
 
     <div class="panel-body">
         
         <div class="row">
             <div class="col-lg-5">
-                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <p><?= Yii::t("employer", "Please fill out the following fields to login:") ?></p>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'fieldConfig' => [
+                        'template' => $fieldTemplate,
+                    ],
+                ]); ?>
                 
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                
+                <?= $form->field($model, 'email')->input("email", ['placeholder' => "email@company.com"]) ?>
+                <?= $form->field($model, 'password')->passwordInput(['placeholder' => "***"]) ?>
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                
+                
                 <div style="color:#999;margin:1em 0">
                     <?= Yii::t("employer", "If you forgot your password you can") ?>
                     <?= Html::a(Yii::t("employer",'reset it'), ['site/request-password-reset']) ?>
                 </div>
+                
+                
                 <div class="form-group">
                 <?= Html::submitButton(Yii::t("employer",'Login'), ['class' => 'btn btn-teal', 'name' => 'login-button']) ?>
                 </div>
