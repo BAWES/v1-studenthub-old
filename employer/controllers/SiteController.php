9e6e423e1d4050a51a0f05d19110eb0d3cca9403
BAWES <BAWES@192.168.1.113>
2015-05-03 21:09:30 +0300

    Set i18n for password reset messages

--- employer/controllers/SiteController.php
+++ employer/controllers/SiteController.php
@@ -172,7 +172,7 @@ class SiteController extends Controller
         }
 
         if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
-            Yii::$app->getSession()->setFlash('success', 'New password was saved.');
+            Yii::$app->getSession()->setFlash('success', Yii::t('employer', 'New password was saved.'));
 
             return $this->goHome();
         }
