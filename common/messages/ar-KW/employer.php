c940c52b48f9bff2b3b46691c09ffb8249ba851a
BAWES <BAWES@192.168.1.113>
2015-05-04 14:18:01 +0300

    Translations

--- common/messages/ar-KW/employer.php
+++ common/messages/ar-KW/employer.php
@@ -17,17 +17,20 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'New password was saved.' => 'تم حفظ كلمة المرور الجديدة.',
-    'Password reset link sent, please check your email for further instructions.' => 'تم ارسال الرابط الى إعادة تعيين كلمة المرور، يرجى التحقق من بريدك الالكتروني لمزيد من التعليمات.',
-    'Sorry, we are unable to reset password for email provided.' => 'آسف، نحن غير قادرين على إعادة تعيين كلمة المرور للبريد الإلكتروني',
+    'Please choose your new password' => 'يرجى اختيار كلمة مرور جديدة',
+    'Reset password' => 'إعادة تعيين كلمة المرور',
+    'Save' => 'حفظ',
     'Don\'t have an account? <a href=\'{url}\'>Register</a>' => 'ليس لديك حساب؟ <a href=\'{url}\'>سجل</a>',
     'If you forgot your password you can' => 'إذا كنت قد نسيت كلمة المرور الخاصة بك يمكنك ',
     'Login' => 'تسجيل الدخول',
+    'New password was saved.' => 'تم حفظ كلمة المرور الجديدة.',
+    'Password reset link sent, please check your email for further instructions.' => 'تم ارسال الرابط الى إعادة تعيين كلمة المرور، يرجى التحقق من بريدك الالكتروني لمزيد من التعليمات.',
     'Please click the verification link sent to you by email to activate your account.<br/><a href=\'{url}\'>Contact us</a> if you need any assistance' => 'الرجاء الضغط على الرابط الذي أرسل لك عن طريق البريد الإلكتروني لتفعيل حسابك.<br/><a href=\'{url}\'>إتصل بنا </a> إذا كنت تحتاج إلى مساعدة',
     'Please fill out the following fields to login' => 'يرجى ملء الحقول التالية للدخول',
     'Please fill out your email. A link to reset password will be sent there' => 'يرجى ملء البريد الإلكتروني الخاص بك. سيتم إرسال رابط لإعادة تعيين كلمة المرور',
     'Request password reset' => 'طلب إعادة تعيين كلمة المرور',
     'Send' => 'إرسال',
+    'Sorry, we are unable to reset password for email provided.' => 'آسف، نحن غير قادرين على إعادة تعيين كلمة المرور للبريد الإلكتروني',
     'There is no employer with such email.' => 'لا يوجد صاحب العمل مع هذا البريد الإلكتروني.',
     'reset it' => 'إعادة تعيينها',
 ];
