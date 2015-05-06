0e0fb482f9131048491475743c3d6ceb3c7ec5a2
Khalid Al-Mutawa <contact@bawes.net>
2015-05-06 18:48:53 +0300

    Contact form layout adjustments

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -7,30 +7,53 @@ use yii\captcha\Captcha;
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \frontend\models\ContactForm */
 
-$this->title = 'Contact';
+$this->title = Yii::t("student", "Contact");
 $this->params['breadcrumbs'][] = $this->title;
+
+$fieldTemplate = "{label}\n{beginWrapper}\n"
+                        . "<div class='inputer'>\n<div class='input-wrapper'>\n"
+                        . "{input}\n"
+                        . "</div>\n</div>\n{hint}\n{error}\n"
+                        . "{endWrapper}";
+$captchaTemplate = "{image}\n"
+                        . "<div class='inputer'>\n<div class='input-wrapper'>\n"
+                        . "{input}\n"
+                        . "</div>\n</div>\n";
+
+//$captchaTemplate = '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>';
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-    </p>
+<div class="panel">
+    <div class="panel-heading">
+        <div class="panel-title">
+            <h4><?= Yii::t('frontend', 'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.') ?></h4>
+        </div>
+    </div>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-                <?= $form->field($model, 'name') ?>
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'subject') ?>
-                <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
-                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                ]) ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+    <div class="panel-body">
+        
+        <div class="row">
+            <div class="col-lg-5 <?= $this->params['isArabic']?"col-lg-offset-7":"" ?>">
+                <?php $form = ActiveForm::begin([
+                    'id' => 'contact-form',
+                    'fieldConfig' => [
+                        'template' => $fieldTemplate,
+                    ],
+                ]); ?>
+                
+                
+                    <?= $form->field($model, 'name') ?>
+                    <?= $form->field($model, 'email') ?>
+                    <?= $form->field($model, 'subject') ?>
+                    <?= $form->field($model, 'body')->textArea(['rows' => 1, 'class' => 'form-control js-auto-size']) ?>
+                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                        'template' => $captchaTemplate,
+                    ]) ?>
+                    <div class="form-group">
+                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    </div>
+                <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 
