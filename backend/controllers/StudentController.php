ebe004432be880afa681c8d6d1da82d04d4baa0f
BAWES <BAWES@192.168.1.113>
2015-05-08 20:37:28 +0300

    Updated view action

--- backend/controllers/StudentController.php
+++ backend/controllers/StudentController.php
@@ -82,8 +82,10 @@ class StudentController extends Controller
      */
     public function actionView($id)
     {
+        $model = $this->findModel($id);
+        
         return $this->render('view', [
-            'model' => $this->findModel($id),
+            'model' => $model,
         ]);
     }
 
