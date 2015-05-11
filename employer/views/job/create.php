8b794d4a96f20d53a77839f2992ce4880e954bf7
Khalid Al-Mutawa <contact@bawes.net>
2015-05-11 13:16:06 +0300

    Updated create form

--- employer/views/job/create.php
+++ employer/views/job/create.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
@@ -11,13 +12,27 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="panel">
+    <div class="panel-heading">
+        <div class="panel-title">
+            <h4>Step 1: Basic Details</h4>
+            <div class="steps-pull-right">
+                <ul class="wizard-steps">
+                    <li class="step" id="step1"><a href="#firstStep" class="btn btn-teal btn-ripple">1<span class="ripple _2 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _3 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _4 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _5 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _6 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _7 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span></a></li>
+                    <li class="step" id="step2"><a href="#secondStep" class="btn btn-white btn-ripple">2</a></li>
+                    <li class="step" id="step3"><a href="#thirdStep" class="btn btn-white btn-ripple">3</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
     <div class="panel-body">
-        
-        <?= $this->render('_form', [
+
+        <?=
+        $this->render('_form', [
             'model' => $model,
-        ]) ?>
-        
+        ])
+        ?>
+
     </div>
 
 </div>
