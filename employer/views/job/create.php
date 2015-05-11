2d669be85b06021b54a1e9ac0efe5cf31b4ee500
Khalid Al-Mutawa <contact@bawes.net>
2015-05-11 14:00:45 +0300

    Added fourth step

--- employer/views/job/create.php
+++ employer/views/job/create.php
@@ -20,6 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <li class="step" id="step1"><a href="#firstStep" class="btn btn-teal btn-ripple">1<span class="ripple _2 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _3 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _4 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _5 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _6 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span><span class="ripple _7 animate" style="height: 28px; width: 28px; top: 4px; left: -1px;"></span></a></li>
                     <li class="step" id="step2"><a href="#secondStep" class="btn btn-white btn-ripple">2</a></li>
                     <li class="step" id="step3"><a href="#thirdStep" class="btn btn-white btn-ripple">3</a></li>
+                    <li class="step" id="step3"><a href="#fourthStep" class="btn btn-white btn-ripple">4</a></li>
                 </ul>
             </div>
         </div>
