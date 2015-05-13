cce64d4576660fb56204ba81cda2f32637a2dfff
Khalid Al-Mutawa <contact@bawes.net>
2015-05-13 13:38:20 +0300

    i18n

--- employer/views/job/step1.php
+++ employer/views/job/step1.php
@@ -14,13 +14,13 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="panel">
     <div class="panel-heading">
         <div class="panel-title">
-            <h4>Step 1: Basic Details</h4>
+            <h4><?= Yii::t("employer", "Step 1: Basic Details") ?></h4>
             <div class="steps-pull-right">
                 <ul class="wizard-steps">
-                    <li class="step" id="step1"><a href="#firstStep" class="btn btn-teal btn-ripple">1</a></li>
-                    <li class="step" id="step2"><a href="#secondStep" class="btn btn-white btn-ripple">2</a></li>
-                    <li class="step" id="step3"><a href="#thirdStep" class="btn btn-white btn-ripple">3</a></li>
-                    <li class="step" id="step3"><a href="#fourthStep" class="btn btn-white btn-ripple">4</a></li>
+                    <li class="step" id="step1"><a href="#firstStep" class="btn btn-teal btn-ripple"><?= Yii::$app->formatter->asInteger(1) ?></a></li>
+                    <li class="step" id="step2"><a href="#secondStep" class="btn btn-white btn-ripple"><?= Yii::$app->formatter->asInteger(2) ?></a></li>
+                    <li class="step" id="step3"><a href="#thirdStep" class="btn btn-white btn-ripple"><?= Yii::$app->formatter->asInteger(3) ?></a></li>
+                    <li class="step" id="step3"><a href="#fourthStep" class="btn btn-white btn-ripple"><?= Yii::$app->formatter->asInteger(4) ?></a></li>
                 </ul>
             </div>
         </div>
