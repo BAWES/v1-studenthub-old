bb9bc6042d403f45b3340b992e9d98f70fbe432d
BAWES <BAWES@192.168.1.113>
2015-05-13 20:33:31 +0300

    Filter model for employer filter selection

---
+++ employer/models/Filter.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "filter".
+ * It extends from \common\models\Filter but with custom functionality for employer job creation
+ * 
+ */
+class Filter extends \common\models\Filter {
+    //majors and languages selected during job filter creation
+    public $majorsSelected;
+    public $languagesSelected;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules() {
+        return array_merge(parent::rules(), [
+            [['majorsSelected', 'languagesSelected'], 'safe'],
+            
+            //Validate Major and Language selections (if selected)
+            ['majorsSelected', '\common\components\ArrayValidator',
+                'rule' => ['exist',
+                    'targetClass' => '\common\models\Major',
+                    'targetAttribute' => 'major_id',
+                    'message' => \Yii::t('frontend', 'Selected major does not exist.')
+                ]
+            ],
+            ['languagesSelected', '\common\components\ArrayValidator',
+                'rule' => ['exist',
+                    'targetClass' => '\common\models\Language',
+                    'targetAttribute' => 'language_id',
+                    'message' => \Yii::t('frontend', 'Selected language does not exist.')
+                ]
+            ],
+        ]);
+    }
+
+    /**
+     * Attribute labels that are inherited are extended here
+     */
+    public function attributeLabels() {
+        return array_merge(parent::attributeLabels(), [
+            'majorsSelected' => Yii::t('app', 'Majors selected'),
+            'languagesSelected' => Yii::t('app', 'Languages selected'),
+        ]);
+    }
+
+    /**
+     * After the Filter is saved for this job, save the related major and language filters
+     * @param type $insert
+     * @param type $changedAttributes
+     */
+    public function afterSave($insert, $changedAttributes) {
+        parent::afterSave($insert, $changedAttributes);
+
+        //Linking selected majors to student
+        if (is_array($this->majorsSelected)) {
+            //Unlink all majors from this Student
+            $this->unlinkAll('majors', true);
+
+            //Link the new majors to this Student
+            foreach ($this->majorsSelected as $majorId) {
+                $major = \common\models\Major::findOne((int) $majorId);
+                if ($major) {
+                    $this->link('majors', $major);
+                }
+            }
+        }
+
+        //Linking selected languages to student
+        if (is_array($this->languagesSelected)) {
+            //Unlink all languages from this Student
+            $this->unlinkAll('languages', true);
+
+            //Link the new majors to this Student
+            foreach ($this->languagesSelected as $languageId) {
+                $language = \common\models\Language::findOne((int) $languageId);
+                if ($language) {
+                    $this->link('languages', $language);
+                }
+            }
+        }
+    }
+
+
+}
