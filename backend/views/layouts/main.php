67ef0a5bcfa97c102269376c1c3b8ba0cfd5992a
BAWES <BAWES@192.168.1.113>
2015-05-22 20:19:15 +0300

    Fixed notification count for pending jobs

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -22,7 +22,7 @@ $numStudentsNeedEmailVerification = Student::find()->where(['student_email_verif
 $numEmployersNeedEmailVerification = Employer::find()->where(['employer_email_verification' => Employer::EMAIL_NOT_VERIFIED])
                                 ->andWhere(['not like', 'employer_support_field', 'Removed'])
                                 ->count();
-$numJobsPending = Job::find()->where(['job_status' => Job::STATUS_DRAFT])->count();
+$numJobsPending = Job::find()->where(['job_status' => Job::STATUS_PENDING])->count();
 
 AppAsset::register($this);
 ?>
