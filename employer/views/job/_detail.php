a9cfc26d99a7cbb46f28693b00d1032c290178b3
BAWES <BAWES@192.168.1.113>
2015-05-22 15:01:15 +0300

    Modal updated with additional details

--- employer/views/job/_detail.php
+++ employer/views/job/_detail.php
@@ -7,63 +7,105 @@ use yii\helpers\Url;
 ?>
 
 <div class="modal-header">
-    <h4 class="modal-title" style="text-align: center">Zain</h4>
+    <h4 class="modal-title" style="text-align: center"><?= $model->employer->employer_company_name ?></h4>
 </div>
 <div class="modal-body">
-    <h3 style="text-align: center; margin-bottom: 1em">Call Center Inbound Agent</h3>
+    <h3 style="text-align: center; margin-bottom: 1em"><?= $model->job_title ?></h3>
     <div class="panel-group accordion" id="accordion">
+        
+        
         <div class="panel">
             <div class="panel-heading">
-                <a class="panel-title" data-toggle="collapse" href="#collapse1">About the Company</a>
+                <a class="panel-title" data-toggle="collapse" href="#collapse1">
+                    <?= Yii::t("employer", "About the Company") ?>
+                </a>
             </div>
             <div id="collapse1" class="panel-collapse collapse">
                 <div class="panel-body">
-                    The Group’s ﬂagship operation was established in 1983 and made history in 1994 by becoming the first telecom operator to launch a commercial GSM service in the region. Zain Kuwait is the country’s leading operator serving 2.6 million customers as of June 2014, reflecting a market share of 36% and offers a nationwide ultra-fast 4G LTE data network that covers the entire population through 1,787 network sites. Through constant development of the telecommunications infrastructure and proactive marketing initiatives, Zain remains committed to offering customers in Kuwait the most dynamic products and services. The foundation of Zain Kuwait’s achievements lies in the company’s ability to inspire its employees to deliver the best and most imaginative services at every level. With an energetic and inspired predominantly Kuwaiti workforce, the company is committed to employing high caliber people as well as nurturing the finest Kuwaiti talent. With a strong human resources and training program that develops and nurtures leaders in the workplace, the company has consistently opened new doors for its dedicated staff.
+                    <?= Yii::$app->formatter->asNtext($model->employer->employer_company_desc) ?>
                 </div>
             </div>
         </div>
+        
+        
         <div class="panel">
             <div class="panel-heading">
-                <a class="panel-title" data-toggle="collapse" href="#collapse2">Responsibilities</a>
+                <a class="panel-title" data-toggle="collapse" href="#collapse2">
+                    <?= Yii::t("employer", "Responsibilities") ?>
+                </a>
             </div>
             <div id="collapse2" class="panel-collapse collapse">
                 <div class="panel-body">
-                    You will be responsible for ensuring commercial consistency of the core Voice & data pricing plans,  as well as owning all pricing elements in Voice (including Prepaid, Postpaid for consumer and corporate, in addition to Roaming, International Voice and International SMS) while balancing between competitiveness and profitability.<br>You will also be responsible for providing support to all marketing segments with financial projections and price related matters.
+                    <?= Yii::$app->formatter->asNtext($model->job_responsibilites) ?>
                 </div>
             </div>
         </div>
+        
+        
         <div class="panel">
             <div class="panel-heading">
-                <a class="panel-title" data-toggle="collapse" href="#collapse3">Qualifications</a>
-            </div>
-            <div id="collapse3" class="panel-collapse collapse">
-                <div class="panel-body">
-                    - Have a Bachelor in MIS, Marketing or Finance<br>
-                    - 1-2 years of relevant experience<br>
-                </div>
-            </div>
-        </div>
-        <div class="panel">
-            <div class="panel-heading">
-                <a class="panel-title" data-toggle="collapse" href="#collapse4">Skills</a>
+                <a class="panel-title" data-toggle="collapse" href="#collapse4">
+                    <?= Yii::t("employer", "Skills") ?>
+                </a>
             </div>
             <div id="collapse4" class="panel-collapse collapse">
                 <div class="panel-body">
-                    - Good knowledge of all products within a product family<br>
-                    - Good level of knowledge of market trends.<br>
-                    - Basic knowledge and use of technical principles, theories and concept.<br>
-                    - Strong analytical skills and problem solving skills.<br>
-                    - The ability to plan.
+                    <?= Yii::$app->formatter->asNtext($model->job_desired_skill) ?>
                 </div>
             </div>
         </div>
+        
+        
         <div class="panel">
             <div class="panel-heading">
-                <a class="panel-title" data-toggle="collapse" href="#collapse5">Compensation</a>
+                <a class="panel-title" data-toggle="collapse" href="#collapse3">
+                    <?= Yii::t("employer", "Other Qualifications") ?>
+                </a>
+            </div>
+            <div id="collapse3" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <?= Yii::$app->formatter->asNtext($model->job_other_qualifications) ?>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="panel">
+            <div class="panel-heading">
+                <a class="panel-title" data-toggle="collapse" href="#collapse5">
+                    <?= Yii::t("employer", "Compensation") ?>
+                </a>
             </div>
             <div id="collapse5" class="panel-collapse collapse">
                 <div class="panel-body">
-                    TBD
+                    <?= Yii::$app->formatter->asNtext($model->job_compensation) ?>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="panel">
+            <div class="panel-heading">
+                <a class="panel-title" data-toggle="collapse" href="#collapse6">
+                    <?= Yii::t("employer", "Targetted Universities") ?>
+                </a>
+            </div>
+            <div id="collapse6" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <ul class="list-material">
+                        <?php foreach ($model->filter->universities as $university) { ?>
+                            <li class="has-action-left">
+                                <div class="list-action-left">
+                                    <?php if ($university->university_logo) { ?>
+                                        <img src="<?= Url::to('@web/images/universities/' . $university->university_logo) ?>" class="face-radius" alt="">
+                                    <?php } ?>
+                                </div>
+                                <div class="list-content">
+                                    <span class="title">&nbsp;<?= $this->params['isArabic'] ? $university->university_name_ar : $university->university_name_en ?></span>
+                                </div>
+                            </li>
+                        <?php } ?>
+                    </ul>
                 </div>
             </div>
         </div>
