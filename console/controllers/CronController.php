b3ec5b1527ec7bd85712173c90efeb55c6ca11a8
BAWES <BAWES@bawess-mbp>
2015-05-23 18:00:32 +0300

    Created functional Cron controller for Console

---
+++ console/controllers/CronController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace console\controllers;
+
+use Yii;
+use yii\helpers\Console;
+use employer\models\Job;
+
+/**
+ * All Cron actions related to this project
+ */
+class CronController extends \yii\console\Controller {
+    
+    /**
+     * This action will be run once a minute
+     */
+    public function actionMinute($var) {
+        
+        echo $var."\n";
+        //$this->stdout($var, Console::FG_RED, Console::BG_YELLOW, CONSOLE::BLINK)."\n";
+        
+        return self::EXIT_CODE_NORMAL;
+    }
+
+}
