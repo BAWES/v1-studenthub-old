6b7a6054d51bb6381cbbc8b5b97e56ab81dace1d
Khalid Al-Mutawa <contact@bawes.net>
2015-05-24 09:37:36 +0300

    Approving a job adds it to the process queue

--- backend/models/Job.php
+++ backend/models/Job.php
@@ -4,6 +4,7 @@ namespace backend\models;
 
 use Yii;
 use yii\db\Expression;
+use common\models\JobProcessQueue;
 
 /**
  * This is the model class for table "job".
@@ -20,6 +21,17 @@ class Job extends \common\models\Job {
         //If Job pending, verify
         if($this->job_status == self::STATUS_PENDING){
             $this->job_status = self::STATUS_OPEN;
+            
+            //Add it to the queue if it hasn't been broadcasted
+            if($this->job_broadcasted == self::BROADCASTED_NO){
+                $queue = new JobProcessQueue();
+                $queue->job_id = $this->job_id;
+                if(!$queue->save()){
+                    print_r($queue->errors);
+                    exit();
+                }
+            }
+            
             $this->save(false);
             
             return true;
