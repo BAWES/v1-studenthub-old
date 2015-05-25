fed682cb02b5de5543a6fe220159f0b722f5d94e
Khalid Al-Mutawa <contact@bawes.net>
2015-05-25 12:06:13 +0300

    Updated processing code

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -25,65 +25,13 @@ class CronController extends \yii\console\Controller {
          */
         $queuedJob = JobProcessQueue::find()->orderBy("queue_datetime ASC")->one();
         if($queuedJob){
+            $studentCount = 0;
             $job = $queuedJob->job;
 
             /**
              * Exit if the job has already been broadcasted
              * otherwise continue processing
              */
-            $this->stdout("\nLooking for qualified Applicants"."\n", Console::FG_GREY);
-            
-            $filter = $job->filter;
-            if($filter){
-                $query = Student::find();
-                
-                /**
-                 * Only verified and non-banned students
-                 */
-                $query->where([
-                    'student_id_verification' => Student::ID_VERIFIED,
-                    'student_email_verification' => Student::EMAIL_VERIFIED,
-                    'student_banned' => Student::BAN_STUDENT_NOT_BANNED,
-                    ]);
-                
-                /**
-                 * Add where conditions based on filter values
-                 */
-                $universityList = $filter->universities;
-                $languageList = $filter->languages;
-                $countryList = $filter->countries;
-                $majorList = $filter->majors;
-                
-                
-                //GPA Filter
-                $query->andFilterWhere(['>=', 'student_gpa', $filter->filter_gpa]);
-                //Graduation Filter
-                $query->andFilterWhere(['>=', 'student_graduating_year', $filter->filter_graduation_year_start]);
-                $query->andFilterWhere(['<=', 'student_graduating_year', $filter->filter_graduation_year_end]);
-                //Transportation Filter
-                if($filter->filter_transportation){
-                    $query->andFilterWhere(['student_transportation' => $filter->filter_transportation]);
-                }
-                //English language filter
-                $query->andFilterWhere(['student_english_level' => $filter->filter_english_level]);
-                //Degree Filter
-                $query->andFilterWhere(['degree_id' => $filter->degree_id]);
-                
-                /**
-                 * Test query and output
-                 */
-                echo $query->createCommand()->rawSql;
-                echo "\n";
-                $students = $query->all();
-                if($students){
-                    foreach($students as $student){
-                        $this->stdout($student->student_email. " record found"."\n", Console::FG_YELLOW);
-                    }
-                }else $this->stdout( "No records found"."\n", Console::FG_YELLOW);
-                
-            }
-            
-            
             if($job->job_broadcasted == Job::BROADCASTED_YES){
                 $this->stdout("ERROR: Job #".$job->job_id." has already been broadcasted"."\n", Console::FG_RED);
             }else{
@@ -101,12 +49,55 @@ class CronController extends \yii\console\Controller {
                  * Find and filter students who qualify, for each student that qualifies - create notification and qualification record
                  */
                 $this->stdout("\nLooking for qualified Applicants"."\n", Console::FG_GREY);
+                
+                /**
+                 * Only verified and non-banned students
+                 */
+                $query = Student::find()->where([
+                        'student_id_verification' => Student::ID_VERIFIED,
+                        'student_email_verification' => Student::EMAIL_VERIFIED,
+                        'student_banned' => Student::BAN_STUDENT_NOT_BANNED,
+                        ]);
+
+                /**
+                 * Add where conditions based on filter values
+                 */
+                $filter = $job->filter;
+                if($filter){
+                    $universityList = $filter->universities;
+                    $languageList = $filter->languages;
+                    $countryList = $filter->countries;
+                    $majorList = $filter->majors;
 
 
-                //Only verified students allowed (both ID and email)
-                //Only non-banned students
-
-
+                    //GPA Filter
+                    $query->andFilterWhere(['>=', 'student_gpa', $filter->filter_gpa]);
+                    //Graduation Filter
+                    $query->andFilterWhere(['>=', 'student_graduating_year', $filter->filter_graduation_year_start]);
+                    $query->andFilterWhere(['<=', 'student_graduating_year', $filter->filter_graduation_year_end]);
+                    //Transportation Filter
+                    if($filter->filter_transportation){
+                        $query->andFilterWhere(['student_transportation' => $filter->filter_transportation]);
+                    }
+                    //English language filter
+                    $query->andFilterWhere(['student_english_level' => $filter->filter_english_level]);
+                    //Degree Filter
+                    $query->andFilterWhere(['degree_id' => $filter->degree_id]);
+                    
+                }
+                
+                /**
+                 * Test query and output
+                 */
+                echo $query->createCommand()->rawSql;
+                echo "\n";
+                $students = $query->all();
+                if($students){
+                    foreach($students as $student){
+                        $studentCount++;
+                        $this->stdout($student->student_email. " record found"."\n", Console::FG_YELLOW);
+                    }
+                }else $this->stdout( "No records found"."\n", Console::FG_YELLOW);
 
                 /**
                  * Set job_broadcasted to BROADCASTED_YES when the broadcast is complete
@@ -123,7 +114,7 @@ class CronController extends \yii\console\Controller {
             $this->stdout("\nRemoving Job from Queue"."\n", Console::FG_RED);
             //Remove comment once implementation is complete
             //$queuedJob->delete();
-            $this->stdout("Complete"."\n", Console::FG_GREEN);
+            $this->stdout("Complete, broadcasted job to $studentCount students"."\n", Console::FG_GREEN);
         }else $this->stdout("There are no jobs in the Queue"."\n", Console::FG_RED);
         
         return self::EXIT_CODE_NORMAL;
