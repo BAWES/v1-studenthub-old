085a4c7d4d7215997c4f6c1eeccad0e55ced0718
Khalid Al-Mutawa <contact@bawes.net>
2015-05-26 12:41:53 +0300

    Re-designed qualifications and notifications to use batch insert rather than one by one

--- backend/models/Job.php
+++ backend/models/Job.php
@@ -47,39 +47,71 @@ class Job extends \common\models\Job {
      * Broadcasts the job to qualified students
      * @return int number of students broadcasted to
      */
-    public function broadcast(){
+    public function broadcast() {
         $studentCount = 0;
         
         /**
-        * Delete all existing student notifications and qualifications for this job (if exists)
-        */
+         * Delete all existing student notifications and qualifications for this job (if exists)
+         */
         StudentJobQualification::deleteAll(['job_id' => $this->job_id]);
         NotificationStudent::deleteAll(['job_id' => $this->job_id]);
 
         /**
          * Find and filter students who qualify, for each student that qualifies - create notification and qualification record
          */
-
-        $students = $this->getQualifiedStudents()->all();
-        if($students){
+        $qualification = new StudentJobQualification();
+        $notification = new NotificationStudent();
+        
+        //Set default values
+        //$qualification->qualification_datetime = new Expression("NOW()");
+        //$notification->notification_datetime = new Expression("NOW()");
+        $notification->notification_sent = NotificationStudent::SENT_FALSE;
+        $notification->notification_viewed = NotificationStudent::VIEWED_FALSE;
+        
+        foreach($this->getQualifiedStudents()->batch() as $students){
+            $batchCount = 0;
+            $qualifyStudents = [];
+            $notifyStudents = [];
+            
             foreach($students as $student){
                 /**
-                 * Create notification and qualification records for this student
+                 * Set Job Qualification for this student and append its attributes to the array for batch insert
                  */
+                $qualifyStudents[] = [$this->job_id, $student->student_id, new Expression("NOW()")];
 
+                /**
+                 * Create notification for this student and append its attributes to the array for batch insert
+                 */
+                $notifyStudents[] = [$this->job_id, $student->student_id, NotificationStudent::SENT_FALSE, NotificationStudent::VIEWED_FALSE, new Expression("NOW()")];
 
-
-
+                /**
+                 * Update student count for this job
+                 */
                 $studentCount++;
+                $batchCount++;
             }
-
+            
             /**
-            * Set job_broadcasted to BROADCASTED_YES when the broadcast is complete
-            */
-           Yii::info("Broadcasted Job #".$this->job_id." to $studentCount students", __METHOD__);
-           $this->job_broadcasted = self::BROADCASTED_YES;
-           $this->save(false);
-
+             * Insert the batch to DB if it has records
+             */
+            if($batchCount > 0){
+                Yii::$app->db->createCommand()->batchInsert(StudentJobQualification::tableName(), 
+                        ['job_id', 'student_id', 'qualification_datetime'], 
+                        $qualifyStudents)->execute();
+                Yii::$app->db->createCommand()->batchInsert(NotificationStudent::tableName(), 
+                        ['job_id', 'student_id', 'notification_sent', 'notification_viewed', 'notification_datetime'], 
+                        $notifyStudents)->execute();
+            }
+        }
+        
+        
+        /**
+         * Set job_broadcasted to BROADCASTED_YES when the broadcast is complete
+         */
+        if($studentCount > 0){
+            Yii::info("Broadcasted Job #".$this->job_id." to $studentCount students", __METHOD__);
+            $this->job_broadcasted = self::BROADCASTED_YES;
+            $this->save(false);
         }else Yii::warning("Job #".$this->job_id." has no qualified students", __METHOD__);
             
 
