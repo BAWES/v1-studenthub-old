83983ddf4ecdfc6957f0a4e67a67522a5989e40d
Khalid Al-Mutawa <contact@bawes.net>
2015-05-28 12:42:08 +0300

    Added JobQuery for additional filtering

---
+++ common/models/JobQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\Expression;
+
+/**
+ * StudentQuery extends ActiveQuery, allowing easier filtering of students
+ */
+class JobQuery extends \yii\db\ActiveQuery{
+    
+    /**
+     * Active Jobs Only
+     * (Status Open or Status Pending) AND have already been broadcasted
+     */
+    public function active()
+    {
+        return $this->where(['in', 'job_status', [Job::STATUS_OPEN, JOB::STATUS_PENDING]])
+                    ->andWhere(['job_broadcasted' => Job::BROADCASTED_YES]);
+    }
+    
+}
