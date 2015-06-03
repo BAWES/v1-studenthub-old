551a6c144be8eb49604a65e3e30ce2503cec3491
Khalid Al-Mutawa <contact@bawes.net>
2015-06-03 12:24:18 +0300

    Added weird script

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -77,6 +77,7 @@ $this->registerJs($jsInclude, View::POS_READY, 'my-options');
         <meta name="msapplication-wide310x150logo" content="<?= Url::to('@web/images/icons') ?>/widetile.png" />
         <meta name="msapplication-square310x310logo" content="<?= Url::to('@web/images/icons') ?>/largetile.png" />
         <!-- END SHORTCUT AND TOUCH ICONS -->
+        <script>(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")</script>   
         
         <?php $this->head() ?>
     </head>
