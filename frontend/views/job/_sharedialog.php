cb39a155756155491d3805c589982d5a9fec31d8
Khalid Al-Mutawa <contact@bawes.net>
2015-06-09 10:16:43 +0300

    Added twitter and linkedin share functionalities

--- frontend/views/job/_sharedialog.php
+++ frontend/views/job/_sharedialog.php
@@ -20,12 +20,12 @@ $shareUrl = Url::to(['job/share', 'id' => $model->job_id], true);
                 </a>                                                
             </div>
             <div class="col-xs-4">
-                <a href='#shareLink' target='_blank' class="btn btn-twitter">
+                <a href="javascript:twtShare('<?= $shareUrl ?>', 'Job opening at <?= Html::encode($model->employer->employer_company_name) ?> on @studenthubco')" class="btn btn-twitter">
                     <i class="fa fa-twitter"></i>
                 </a>                                                
             </div>
             <div class="col-xs-4">
-                <a href='#shareLink' target='_blank' class="btn btn-linkedin">
+                <a href="javascript:lnkdShare('<?= $shareUrl ?>', '<?= Html::encode($model->job_title) ?>', '<?= Html::encode($model->job_responsibilites) ?>')" target='_blank' class="btn btn-linkedin">
                     <i class="fa fa-linkedin"></i>
                 </a> 
             </div>
@@ -42,5 +42,16 @@ function fbShare(url, title, descr, image, winWidth, winHeight) {
     var winTop = (screen.height / 2) - (winHeight / 2);
     var winLeft = (screen.width / 2) - (winWidth / 2);
     window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+}
+
+function twtShare(url, text) {
+  window.open( "http://twitter.com/share?url=" + 
+    encodeURIComponent(url) + "&text=" + 
+    encodeURIComponent(text) + "&count=none/", 
+    "tweet", "height=300,width=550,resizable=1" );
+}
+
+function lnkdShare(url, title, summary) {
+  window.open( "https://www.linkedin.com/shareArticle?mini=true&url="+url+"&title="+ title +"&source=StudentHub&summary="+summary, "linkedin", "height=300,width=550,resizable=1"); 
 }
 </script>
