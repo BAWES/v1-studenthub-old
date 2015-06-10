5973cd02819fcc7c00592d9bd72dbd074cb88fad
Khalid Al-Mutawa <contact@bawes.net>
2015-06-10 09:15:35 +0300

    Background color updates

--- employer/views/layouts/main.php
+++ employer/views/layouts/main.php
@@ -152,7 +152,7 @@ $this->registerJs($jsInclude, View::POS_READY, 'my-options');
         <div class="content" <?= $this->params['isArabic'] ? 'style="direction:rtl"' : '' ?>>
 
             <div class="page-header full-content bg-teal" style="<?= isset($this->blocks['header-tabs']) ? 'min-height: 141px;' : '' ?>
-                 background-image: url(<?= Url::to('@web/images/pattern.jpg') ?>)">
+                 background: #446f65 url(<?= Url::to('@web/images/pattern.jpg') ?>)">
                 <div class="row">
                     <div class="col-sm-6">
                         <?php /*<h1><?= Html::encode($this->title) ?></h1>*/ ?>
