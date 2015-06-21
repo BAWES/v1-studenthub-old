2718f20ac40222814fbb1e78fe5362da78c41b0c
Khalid Al-Mutawa <contact@bawes.net>
2015-06-21 11:44:35 +0300

    RTL layout of share dialog btn

--- employer/views/dashboard/_job.php
+++ employer/views/dashboard/_job.php
@@ -14,7 +14,7 @@ use common\models\Job;
             <h3 class="card-title"><?= $model->job_title? $model->job_title : Yii::t("employer", "No Title Set") ?></h3>
             <?php if($model->job_status != Job::STATUS_DRAFT) { ?>
             <a class="btn btn-danger btn-ripple jobShare" data-job="<?= Yii::$app->urlManagerFrontend->createUrl(['job/share-dialog', 'id' => $model->job_id]) ?>"
-               style='position: absolute; bottom:0; left:0;' data-toggle="modal" data-target="#shareDialog"><span class="ion-android-share-alt"></span></a>
+               style='position: absolute; bottom:0; <?= $this->params['isArabic']?"right":"left"?>:0;' data-toggle="modal" data-target="#shareDialog"><span class="ion-android-share-alt"></span></a>
             <?php } ?>
         </div><!--.card-heading-->
 
