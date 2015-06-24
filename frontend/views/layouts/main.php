e6619a6138cfaa60e9bf002dea765311da3d4e79
Khalid Al-Mutawa <contact@bawes.net>
2015-06-24 12:57:04 +0300

    Adds google analytics

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,6 +26,19 @@ else{
 //Include Modernizr in head section
 $this->registerJsFile(Url::to('@web/plugins/modernizr/modernizr.min.js'), ['position' => View::POS_HEAD]);
 
+//Google Analytics JS
+$analytics = "
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64467043-1', 'auto');
+  ga('send', 'pageview');
+";
+$this->registerJs($analytics);
+
+
 //Initialize on Document Ready (via jQuery)
 $jsInclude = "
 Pleasure.init();
