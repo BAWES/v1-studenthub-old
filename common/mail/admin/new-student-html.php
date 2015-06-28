1ba207e97ce920ab342bd5cb9fadfe5c30595a97
Khalid Al-Mutawa <contact@bawes.net>
2015-06-28 14:55:46 +0300

    Notify admins on new student signup

---
+++ common/mail/admin/new-student-html.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $student common\models\Student */
+
+?>
+<tr>
+    <td>
+        <h1>New Student has signed up</h1>
+        <p class="lead">
+            Student Name: <?= $student->student_firstname ?> <?= $student->student_lastname ?> <br/>
+            Student Phone: <?= $student->student_contact_number ?> <br/>
+            Student Email: <?= $student->student_email ?> <br/>
+            ID Verification: <?= $student->idVerificationStatus ?> <br/>
+        </p>
+    </td>
+    <td class="expander"></td>
+</tr>
