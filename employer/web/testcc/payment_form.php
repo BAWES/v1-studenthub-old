9818252991cf84253345b22784654d520ad05d79
Khalid Al-Mutawa <contact@bawes.net>
2015-07-01 13:43:07 +0300

    Minor adjustments to layout of cc process

--- employer/web/testcc/payment_form.php
+++ employer/web/testcc/payment_form.php
@@ -1,45 +1,40 @@
 <?php include 'security.php' ?>
 <?php
 $params = [];
-/**
- * NBK BANK AND TRANSACTION DETAILS
- */
+//NBK BANK AND TRANSACTION DETAILS
 $params['access_key'] = "574f600374cf368db32d6328c0528741";
 $params['profile_id'] = "nbk_bawes_acct";
 $params['transaction_type'] = "authorization";
 
-/**
- * LOCAL DETAILS - can be either en-US or ar-XN
- */
+//LOCAL DETAILS - can be either en-US or ar-XN
 $params['locale'] = "en";
 
-/**
- * Unique Track ID for each transaction, no two transactions can have this same ID
- */
+//Unique Track ID for each transaction, no two transactions can have this same ID
 $params['transaction_uuid'] = uniqid();
 
-/**
- * Unique Reference number, we will use this to know who paid for what (references our db column)
- */
+//Unique Reference number, we will use this to know who paid for what (references our db column)
 $params['reference_number'] = "123";
 
-/**
- * Unsigned fields that a user will be able to edit
- */
+//Unsigned fields that a user will be able to edit
 $params['unsigned_field_names'] = "bill_to_forename,bill_to_surname,bill_to_email,bill_to_phone";
 $params['amount'] = "100.000";
 $params['currency'] = "KWD";
 
-/**
- * Fields that should be signed to protect from tampering
- */
+//Fields that should be signed to protect from tampering
 $params['signed_field_names'] = "access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency";
 $params['signed_date_time'] = gmdate("Y-m-d\TH:i:s\Z");
 
-/**
- * The Signing of the required params
- */
+//The Signing of the required params
 $signature = sign($params);
+
+
+/**
+ * Unsigned Data
+ */
+$firstName = "noreal";
+$lastName = "name";
+$email = "null@cybersource.com";
+$phone = "99811042";
 
 ?>
 <html>
@@ -67,10 +62,10 @@ $signature = sign($params);
     
     <!-- Unsigned fields that a user will be able to edit -->
     <input type="hidden" name="unsigned_field_names" value="<?= $params['unsigned_field_names'] ?>">
-    <input type="hidden" name="bill_to_forename" value="noreal">
-    <input type="hidden" name="bill_to_surname" value="name">
-    <input type="hidden" name="bill_to_email" value="khalid@bawes.net">
-    <input type="hidden" name="bill_to_phone" value="99811042">
+    <input type="hidden" name="bill_to_forename" value="<?= $firstName ?>">
+    <input type="hidden" name="bill_to_surname" value="<?= $lastName ?>">
+    <input type="hidden" name="bill_to_email" value="<?= $email ?>">
+    <input type="hidden" name="bill_to_phone" value="<?= $phone ?>">
     
     <!-- Payment Details -->
     <input type="hidden" name="amount" value="<?= $params['amount'] ?>">
