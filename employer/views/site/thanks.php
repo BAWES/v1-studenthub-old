5239ee6a099a24f9be059506b5e76a16e20ade4c
Khalid Al-Mutawa <contact@bawes.net>
2015-07-05 12:01:39 +0300

    Updates registration thank you page for both employer and student

--- employer/views/site/thanks.php
+++ employer/views/site/thanks.php
@@ -4,16 +4,11 @@ $this->title = Yii::t('register', 'Thank You!');
 $this->params['breadcrumbs'][] = Yii::t('register', 'Thank You!');
 
 ?>
-<div class="panel">
-    <div class="panel-heading">
-        <div class="panel-title">
-            <h4><?= Yii::t('register', 'Thank You!') ?></h4>
-        </div>
-    </div>
 
-    <div class="panel-body">
-
-        <h3><?= Yii::t('register', 'Thanks for signing up, please click on the link sent to you by email to verify your account') ?></h3>
-        
-    </div>
+<div class="card">
+    <div class="panel-body" style="text-align:center;">
+        <h1 style="font-size:60px"><?= Yii::t('register', 'Thank You!') ?></h1>                        
+        <i class="fa fa-envelope" style="font-size:200px; color:grey; margin-top:-20px; padding-bottom: 20px"></i>
+        <p class="sub"><?= Yii::t('register', 'Thanks for signing up, please click on the link sent to you by email to verify your account') ?></p>                                           
+    </div>             
 </div>
