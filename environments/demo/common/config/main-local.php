d0cb087c313dda1a37fc9ccca2f00ad818bf3469
Khalid Al-Mutawa <contact@bawes.net>
2015-07-12 12:41:32 +0300

    Adds schema caching to improve performance

--- environments/demo/common/config/main-local.php
+++ environments/demo/common/config/main-local.php
@@ -1,12 +1,22 @@
 <?php
 return [
     'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=studenthub',
             'username' => 'studenthubuser',
             'password' => 'jxwMFGBJb3LzrypS',
             'charset' => 'utf8',
+            
+            //Enable schema caching
+            'enableSchemaCache' => true,
+            // Duration of schema cache.
+            'schemaCacheDuration' => 3600, // 1 hour
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
