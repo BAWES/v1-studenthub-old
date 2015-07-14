57f6b621fe825d1d76e4bda556659bcd3b7ad541
BAWES <BAWES@192.168.0.22>
2015-07-14 20:43:03 +0300

    Only process demo function if the params say its a demo, stops destruction of live server

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -60,63 +60,64 @@ class CronController extends \yii\console\Controller {
      * Resets the demo to default state
      */
     public function actionResetDemo(){
-        $demoEmployerId = 14;
-        $demoStudentId = 24;
-        $demoJobId = 49;
-        
-        /**
-         * Delete all job applications except for the ones made for demo (49)
-         */
-        StudentJobApplication::deleteAll("job_id != $demoJobId");
-        
-        /**
-         * Delete all job applications and notifications made by the demo student account (24)
-         */
-        StudentJobApplication::deleteAll("student_id = $demoStudentId");
-        NotificationEmployer::deleteAll("student_id = $demoStudentId");
-        
-        /**
-         * Update Timestamp on all Notifications, set to now
-         */
-        NotificationEmployer::updateAll(['notification_datetime' => new Expression('NOW()')]);
-        NotificationStudent::updateAll(['notification_datetime' => new Expression('NOW()')]);
-        
-        
-        /**
-         * Set number of applicants to zero for all jobs except for one by demo (49)
-         */
-        Job::updateAll(['job_current_num_applicants' => 0], "job_id != $demoJobId");
-        
-        /**
-         * Set the demo job to have 3 applicants only
-         */
-        $demoJob = Job::findOne($demoJobId);
-        $demoJob->job_current_num_applicants = 3;
-        $demoJob->save(false);
-        
-        /**
-         * Mark all notifications as "Unread"
-         */
-        NotificationStudent::updateAll(['notification_viewed' => NotificationStudent::VIEWED_FALSE]);
-        NotificationEmployer::updateAll(['notification_viewed' => NotificationEmployer::VIEWED_FALSE]);
-        
-        /*
-         * Delete all jobs/filters/relations that belong to demo account except for demo one (49)
-         */
-        $demoJobs = Job::find()->where(['employer_id' => $demoEmployerId])
-                                ->andWhere("job_id != $demoJobId")
-                                ->all();
-        foreach($demoJobs as $job){
-            //Clear all details linked to this job then delete this job
-            $job->unlinkAll('notificationEmployers', true);
-            $job->unlinkAll('notificationStudents', true);
-            $job->unlinkAll('studentJobApplications', true);
-            $job->unlinkAll('studentJobQualifications', true);
-            $job->unlinkAll('payments', true);
-            $job->unlinkAll('notificationEmployers', true);
-            $job->delete();
+        if(Yii::$app->params['isDemo']){
+            $demoEmployerId = 14;
+            $demoStudentId = 24;
+            $demoJobId = 49;
+
+            /**
+             * Delete all job applications except for the ones made for demo (49)
+             */
+            StudentJobApplication::deleteAll("job_id != $demoJobId");
+
+            /**
+             * Delete all job applications and notifications made by the demo student account (24)
+             */
+            StudentJobApplication::deleteAll("student_id = $demoStudentId");
+            NotificationEmployer::deleteAll("student_id = $demoStudentId");
+
+            /**
+             * Update Timestamp on all Notifications, set to now
+             */
+            NotificationEmployer::updateAll(['notification_datetime' => new Expression('NOW()')]);
+            NotificationStudent::updateAll(['notification_datetime' => new Expression('NOW()')]);
+
+
+            /**
+             * Set number of applicants to zero for all jobs except for one by demo (49)
+             */
+            Job::updateAll(['job_current_num_applicants' => 0], "job_id != $demoJobId");
+
+            /**
+             * Set the demo job to have 3 applicants only
+             */
+            $demoJob = Job::findOne($demoJobId);
+            $demoJob->job_current_num_applicants = 3;
+            $demoJob->save(false);
+
+            /**
+             * Mark all notifications as "Unread"
+             */
+            NotificationStudent::updateAll(['notification_viewed' => NotificationStudent::VIEWED_FALSE]);
+            NotificationEmployer::updateAll(['notification_viewed' => NotificationEmployer::VIEWED_FALSE]);
+
+            /*
+             * Delete all jobs/filters/relations that belong to demo account except for demo one (49)
+             */
+            $demoJobs = Job::find()->where(['employer_id' => $demoEmployerId])
+                                    ->andWhere("job_id != $demoJobId")
+                                    ->all();
+            foreach($demoJobs as $job){
+                //Clear all details linked to this job then delete this job
+                $job->unlinkAll('notificationEmployers', true);
+                $job->unlinkAll('notificationStudents', true);
+                $job->unlinkAll('studentJobApplications', true);
+                $job->unlinkAll('studentJobQualifications', true);
+                $job->unlinkAll('payments', true);
+                $job->unlinkAll('notificationEmployers', true);
+                $job->delete();
+            }
         }
-        
         
         return self::EXIT_CODE_NORMAL;
     }
