cd1f37181fba2d096923feb2cb37924208641894
BAWES <BAWES@192.168.1.101>
2015-07-21 09:37:11 +0300

    Adds Bawes to employer list on studenthub

--- employer/views/site/employers.php
+++ employer/views/site/employers.php
@@ -56,6 +56,17 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer5.jpg") ?>" style="width:100%" alt="BAWES - Built Awesome">
+                </div>
+            </div>
+        </div>
+    </div>
+    
     
 </div>
 
