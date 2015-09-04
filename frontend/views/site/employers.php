700b090c9141d40c5606831eaef374ad2723afb6
BAWES <BAWES@192.168.1.100>
2015-09-04 21:33:17 +0300

    Add kuwait events as employer

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -166,6 +166,17 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer15.jpg") ?>" style="width:100%" alt="Kuwait Events">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 
