688ac2d0ffce6c3361d5e211ddc5f18c4758fc21
BAWES <BAWES@192.168.0.24>
2015-09-05 20:00:47 +0300

    Adds juiced and ar-tattoo to employer list

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -177,6 +177,28 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer16.jpg") ?>" style="width:100%" alt="Arabic Tattoo">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer17.jpg") ?>" style="width:100%" alt="Juiced Refreshments Est.">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 
