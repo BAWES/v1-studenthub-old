bceb52c105ef1e8b0d5f7d9ecca59d723698694a
BAWES <BAWES@192.168.0.24>
2015-09-06 09:50:29 +0300

    Add 3dpme as employer

--- employer/views/site/employers.php
+++ employer/views/site/employers.php
@@ -199,6 +199,17 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer18.jpg") ?>" style="width:100%" alt="3DPme">
+                </div>
+            </div>
+        </div>
+    </div>
+    
     
 </div>
 
