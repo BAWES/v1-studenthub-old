8d92f1d2a2c154ed9568bf50863805a193c46f1a
BAWES <BAWES@192.168.0.21>
2015-09-22 22:58:56 +0300

    Kwtmag added to employer list

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -127,6 +127,17 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="panel">
             <div class="panel-heading">
                 <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer41.jpg") ?>" style="width:100%" alt="KwtMagazine">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
                     <img src="<?= Url::to("@web/images/employer-list/employer5.jpg") ?>" style="width:100%" alt="BAWES - Built Awesome">
                 </div>
             </div>
