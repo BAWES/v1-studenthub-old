f72e7541f90c0745ee921d1d2176f7efb7d23cf3
BAWES <BAWES@192.168.0.21>
2015-09-24 19:14:30 +0300

    Added solo pizza

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -463,6 +463,17 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer42.jpg") ?>" style="width:100%" alt="SOLO Pizza Napulitana">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 
