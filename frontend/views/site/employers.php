25201c33d585079f4faa5c68ec53e08fd62d4903
BAWES <BAWES@192.168.0.21>
2015-09-27 20:52:29 +0300

    Added dopio

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -479,6 +479,17 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="panel">
             <div class="panel-heading">
                 <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer44.jpg") ?>" style="width:100%" alt="Doppio">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
                     <img src="<?= Url::to("@web/images/employer-list/employer43.jpg") ?>" style="width:100%" alt="Kilshay">
                 </div>
             </div>
