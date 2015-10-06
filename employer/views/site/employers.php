5cc563e617853e42206f7d48c4bbc6a9d66f8dca
BAWES <BAWES@192.168.0.11>
2015-10-06 22:39:22 +0300

    Added mubaader and blbayt

--- employer/views/site/employers.php
+++ employer/views/site/employers.php
@@ -551,6 +551,28 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer50.jpg") ?>" style="width:100%" alt="Bilbayt.com">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer51.jpg") ?>" style="width:100%" alt="Mubaader Services">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 <div class="panel" style="text-align:center; padding-top:0.5em; padding-bottom:1em">
