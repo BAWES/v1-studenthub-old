86df7b6a2a016c6cd53e7fea5c10869727a11ec4
BAWES <BAWES@192.168.0.11>
2015-10-07 11:10:45 +0300

    Add Scientific Centre to employer list

--- employer/views/site/employers.php
+++ employer/views/site/employers.php
@@ -573,6 +573,17 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer52.jpg") ?>" style="width:100%" alt="The Scientific Center Management Company">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 <div class="panel" style="text-align:center; padding-top:0.5em; padding-bottom:1em">
