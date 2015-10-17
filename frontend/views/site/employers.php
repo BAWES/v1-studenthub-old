326677f1a19e77b91f4c6182391fad378ac13174
BAWES <BAWES@192.168.0.12>
2015-10-17 09:37:49 +0300

    Add Make to Employer List

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -639,6 +639,17 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer58.jpg") ?>" style="width:100%" alt="Make">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 
