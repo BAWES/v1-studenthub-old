3026ade16c67d342a2f2a53bdc53ba7165e344ac
BAWES <BAWES@192.168.0.12>
2015-10-19 13:45:43 +0300

    Added Ajar and Q8Desserts to employer list

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -650,6 +650,28 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
     
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer59.jpg") ?>" style="width:100%" alt="Ajar Online General Trading Co.">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer60.jpg") ?>" style="width:100%" alt="Q8Desserts">
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 
 
