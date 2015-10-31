a29a3cdaf9649f6723143431b09ba2f8f41bae7f
BAWES <BAWES@bawess-mbp.zain>
2015-10-31 14:35:31 +0300

    Added Wana Social to employer list

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -143,6 +143,17 @@ $this->registerMetaTag([
         <div class="panel">
             <div class="panel-heading">
                 <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer63.jpg") ?>" style="width:100%" alt="WANA Social">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
                     <img src="<?= Url::to("@web/images/employer-list/employer41.jpg") ?>" style="width:100%" alt="KwtMagazine">
                 </div>
             </div>
