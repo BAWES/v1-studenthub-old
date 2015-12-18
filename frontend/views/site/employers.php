dead1c2772e40f33033fb6562e75997acfbe5464
Khalid <khalid@studenthub.co>
2015-12-18 21:12:05 +0300

    Add Alghanim to employer list

--- frontend/views/site/employers.php
+++ frontend/views/site/employers.php
@@ -143,6 +143,17 @@ $this->registerMetaTag([
         <div class="panel">
             <div class="panel-heading">
                 <div class="panel-title">
+                    <img src="<?= Url::to("@web/images/employer-list/employer83.jpg") ?>" style="width:100%" alt="Alghanim Industries">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Logo -->
+    <div class="col-md-2 col-sm-3 col-xs-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <div class="panel-title">
                     <img src="<?= Url::to("@web/images/employer-list/employer81.jpg") ?>" style="width:100%" alt="Bayan Medical Company">
                 </div>
             </div>
