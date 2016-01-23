5be635568b1fd1885001f683d6f9cbb2a49ddfc2
Khalid <khalid@studenthub.co>
2016-01-23 21:31:44 +0800

    Add JPEG extension to uploaded file validation

--- frontend/controllers/RegisterController.php
+++ frontend/controllers/RegisterController.php
@@ -156,7 +156,7 @@ class RegisterController extends \yii\web\Controller {
         
         if($uploadedFile){
             $model = DynamicModel::validateData(compact('uploadedFile'), [
-                [['uploadedFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'jpg, png, gif', 'maxSize' => 10000000,
+                [['uploadedFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'jpg, png, gif, jpeg', 'maxSize' => 10000000,
                     'wrongExtension' => Yii::t('register', 'Only files with these extensions are allowed for ID: {extensions}')],
             ]);
 
@@ -232,7 +232,7 @@ class RegisterController extends \yii\web\Controller {
         
         if($uploadedFile){
             $model = DynamicModel::validateData(compact('uploadedFile'), [
-                [['uploadedFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'jpg, png, gif', 'maxSize' => 10000000,
+                [['uploadedFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'jpg, png, gif, jpeg', 'maxSize' => 10000000,
                     'wrongExtension' => Yii::t('register', 'Only files with these extensions are allowed for your Photo: {extensions}')],
             ]);
 
