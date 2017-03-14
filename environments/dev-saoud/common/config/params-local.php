ba0f3953e9f564d4b2ba94ab29f3c8b49615bdbd
Khalid <khalid@bawes.net>
2017-03-14 17:24:51 +0300

    Delete saoud environment files

--- environments/dev-saoud/common/config/params-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'KNETResourcePath' => '/Users/BAWES/Sites/studenthub/common/components/knet-testing/',
-    'cybersourceEnabled' => true,
-    'knetEnabled' => true,
-    'isDemo' => false,
-    'adminEmail' => 'contact@studenthub.co',
-    'supportEmail' => 'contact@studenthub.co',
-];
